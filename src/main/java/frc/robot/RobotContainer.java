9cacb3a156589ce7962128546949ff51edf34bbc
Sam Perlmutter <sam.perlmutter227@gmail.com>
2024-07-12 13:51:43 -0400

    Move Constants to constants package

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.constants.Constants;
 
 
 /**
